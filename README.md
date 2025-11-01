e9834e49fdba3cefc9d1f6f229d4bc86ef9f97c0
jazzpu <jasmine.theseira@gmail.com>
2025-11-01 19:07:56 +0700

    initializing database

--- README.md
+++ README.md
@@ -10,4 +10,8 @@ pip install -r requirements.txt
 npm install
 
 <!-- To start project -->
+<!-- These docker container and volume names can be changed due to your satisfactory ! -->
+docker pull mongo
+docker volume create mongo-data
+docker run -d -p 27017:27017 --name my-mongo-db -v mongo-data:/data/db mongo
 Run Flask + Tailwind together ==> npm run dev
---
+++ backend/db.py
@@ -0,0 +1,29 @@
+from pymongo import MongoClient, DESCENDING
+from bson import ObjectId
+import os
+
+client = None
+db = None
+
+def initi_db(app):
+    global client, db
+    mongo_uri = app.config.get('MONGO_URI')
+
+    db_name = app.config.get('MONGO_DBNAME', 'ipa_project_db')
+
+
+    if not mongo_uri:
+        print("WARNING: MONGO_URI not set in app config. Using default localhost connection.")
+        mongo_uri = 'mongodb://localhost:27017/'
+    try:
+        client = MongoClient(mongo_uri)
+        db = client[db_name]
+
+        client.admin.command('ping')
+        print(f"Connected to MongoDB at {mongo_uri}, using database '{db_name}'")
+
+    except Exception as e:  
+        print(f"Error connecting to MongoDB: {e}")
+        client = None
+        db = None
+
